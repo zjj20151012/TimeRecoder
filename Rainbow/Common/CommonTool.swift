7f0894495a8cbc4622ae91b683202ae549189edb
zhujiajie <zhujiajie@corp-ci.com>
2017-09-28 00:08:21 +0800

    initial project

---
+++ Rainbow/Common/CommonTool.swift
@@ -0,0 +1,12 @@
+//
+//  CommonTool.swift
+//  Rainbow
+//
+//  Created by 朱佳杰 on 2017/9/27.
+//  Copyright © 2017年 corp-ci. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ Rainbow/RainbowMain/View/RainbowView.swift
@@ -0,0 +1,13 @@
+//
+//  RainbowView.swift
+//  Rainbow
+//
+//  Created by 朱佳杰 on 2017/9/27.
+//  Copyright © 2017年 corp-ci. All rights reserved.
+//
+
+import UIKit
+
+class RainbowView: UIView {
+    
+}
